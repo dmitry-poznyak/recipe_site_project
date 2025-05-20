af0986abbff46c575ac49262da60114b0a31f1d9
dmitry-poznyak <dimapoznyak1227@gmail.com>
2025-05-20 20:49:03 +0300

    исправил добавление категорий

--- recipe/forms.py
+++ recipe/forms.py
@@ -3,6 +3,13 @@ from .models import Recipe, Category
 
 
 class RecipeForm(forms.ModelForm):
+    categories = forms.ModelMultipleChoiceField(
+        queryset=Category.objects.all(),
+        widget=forms.SelectMultiple(attrs={'class': 'form-control'}),
+        label='Категория',
+        required=False
+    )
+
     class Meta:
         model = Recipe
         fields = ['title', 'description', 'steps', 'cooking_time', 'image', 'categories']
@@ -12,9 +19,7 @@ class RecipeForm(forms.ModelForm):
             'steps': 'Шаги приготовления',
             'cooking_time': 'Время приготовления (минуты)',
             'image': 'Изображение',
-            'categories': 'Категория',
         }
         widgets = {
-            'categories': forms.SelectMultiple(attrs={'class': 'form-control'}),
             'steps': forms.Textarea(attrs={'rows': 4}),
         }
